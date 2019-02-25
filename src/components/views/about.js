d01e942c7a54d0d2c43b5478b7c2af4673ddcdcf
Colleen Rooney <crooney@pdx.edu>
2019-02-25 09:34:24 -0800

    syle about page

--- src/components/views/about.js
+++ src/components/views/about.js
@@ -16,7 +16,20 @@ class About extends Component {
         <div className="image">
         </div>
         <div className='bio'>
-          <p>hello</p>
+          <h1>About Me</h1>
+          <hr style={{"width": "90%", "margin": "auto"}}/>
+          <p>Current <strong>Masters of Computer Science</strong> student at Portland 
+            State University. Expected graduation of <strong>March 2020.</strong><br/></p>
+            
+            <p>I discovered my love of coding after graduating with a degree in
+            Biochemistry. I am excited to be a part of building inovative and useful 
+            software. Currently employeed as <em>Research Computing Software and Systems
+             Support Technician.</em> My interest includes, but is not limited to, the
+            the intersection of Molecular Biology and Computer Science.<br/>
+            </p>
+            <p className="internship"><strong>Actively seeking Summer 2019 internships</strong><br/>
+            Will to rellocate for the right opportunity</p>
+      
         </div>
       </div>
       </div>
