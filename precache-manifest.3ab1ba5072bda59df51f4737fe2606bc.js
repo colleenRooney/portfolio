4dd421acb406cdf2a4872638bdb5fa9230e10a4a
Colleen Rooney <crooney@pdx.edu>
2019-02-25 11:09:03 -0800

    Updates

---
+++ precache-manifest.3ab1ba5072bda59df51f4737fe2606bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "570d68063bd1a9b87a99",
+    "url": "/static/css/main.0e7125a5.chunk.css"
+  },
+  {
+    "revision": "570d68063bd1a9b87a99",
+    "url": "/static/js/main.570d6806.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "36dddd4bcb3e65e11453",
+    "url": "/static/css/2.ccb5609e.chunk.css"
+  },
+  {
+    "revision": "36dddd4bcb3e65e11453",
+    "url": "/static/js/2.36dddd4b.chunk.js"
+  },
+  {
+    "revision": "a7d23483b5475d8e255b2875b46cf387",
+    "url": "/static/media/background_2.a7d23483.png"
+  },
+  {
+    "revision": "a67343f819ce096b6bb19ea4ce8ee459",
+    "url": "/index.html"
+  }
+];
